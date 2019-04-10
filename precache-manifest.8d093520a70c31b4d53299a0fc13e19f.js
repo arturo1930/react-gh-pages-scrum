65b76c8af5dd024c414ddbeea5e840e090e3fcc4
HMV <hmv.soluciones@gmail.com>
2019-04-10 11:54:25 -0500

    Updates

---
+++ precache-manifest.8d093520a70c31b4d53299a0fc13e19f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "25b41613f9fa188e1576",
+    "url": "/react-gh-pages-scrum/static/css/main.98699423.chunk.css"
+  },
+  {
+    "revision": "25b41613f9fa188e1576",
+    "url": "/react-gh-pages-scrum/static/js/main.25b41613.chunk.js"
+  },
+  {
+    "revision": "b4af85fa77bf3a172bba",
+    "url": "/react-gh-pages-scrum/static/js/runtime~main.b4af85fa.js"
+  },
+  {
+    "revision": "2914dec7ecb86f6dbaaa",
+    "url": "/react-gh-pages-scrum/static/js/2.2914dec7.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages-scrum/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1a57192560ff5e3e63bf38eab581dc86",
+    "url": "/react-gh-pages-scrum/index.html"
+  }
+];
